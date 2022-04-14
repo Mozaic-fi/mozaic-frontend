27ed2bde3dbf3fa807e9b1f2f97d8405249e3422
Raybi <iiraybi@gmail.com>
2022-04-15 01:39:30 +0600

    add / error pages

---
+++ pages/404.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const custom404 = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='w-100 h-100 df-c'>404 not found</div>
+      </div>
+    </>
+  );
+};
+
+export default custom404;
---
+++ pages/500.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const custom500 = () => {
+  return (
+    <>
+      <div className='container'>
+        <div className='w-100 h-100 df-c'>500 internal error</div>
+      </div>
+    </>
+  );
+};
+
+export default custom500;
