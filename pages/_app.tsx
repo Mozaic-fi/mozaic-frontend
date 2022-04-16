352563716c131072dbf749dee5e0a52e8ae252b5
Raybi <iiraybi@gmail.com>
2022-04-17 04:53:12 +0600

    Add / Web3 Required Config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,6 @@
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+
 import Head from 'next/head';
 import Layout from '../components/layout/Layout';
 import type { AppProps } from 'next/app';
@@ -6,24 +9,27 @@ import '../styles/globals.css';
 import '../styles/commons.css';
 import '../styles/transitions.css';
 import '../styles/layout.css';
-// import '../components/common/nav/Nav.scss';
-// import '../components/products/Card.scss';
-// import './products.scss';
-// import './products/productInDepth.scss';
-
 import { AuthProvider } from '../context/AuthContext';
 import { NotificationProvider } from '../context/NotificationContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const getLibrary = (provider: any) => {
+    const library = new Web3Provider(provider, 'any');
+    library.pollingInterval = 15000;
+    return library;
+  };
+
   return (
     <>
-      <NotificationProvider>
-        <AuthProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </AuthProvider>
-      </NotificationProvider>
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <NotificationProvider>
+          <AuthProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </AuthProvider>
+        </NotificationProvider>
+      </Web3ReactProvider>
       <style jsx global>
         {``}
       </style>
