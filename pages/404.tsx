ad4cf5a5b7952659b3b27d8cfcaae821fae23c8b
Raybi <iiraybi@gmail.com>
2022-04-26 00:33:45 +0600

    ADD / Error Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,58 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
-const custom404 = () => {
+export default function Custom404() {
+  const router = useRouter();
   return (
     <>
-      <div className='container'>
-        <div className='w-100 h-100 df-c'>404 not found</div>
+      <div className='container df-c h-100 w-100'>
+        <div className='df-c'>
+          <div className='text-container'>
+            <p className='error tc-h fs-xxl fw-l'>404</p>
+            <p className='info tc-s fs-xxl fw-l'>Page Not Found</p>
+          </div>
+          <img src='/assets/images/mozgrad.png' alt='' />
+        </div>
+        <p className='link tc-h' onClick={() => router.back()}>
+          Go Back ↩
+        </p>
       </div>
+      <style jsx>{`
+        .container {
+          height: 80vh;
+        }
+
+        .text-container {
+          position: absolute;
+        }
+
+        img {
+          opacity: 0.1;
+          position: relative;
+        }
+        .info {
+          text-align: center;
+          opacity: 0.5;
+          letter-spacing: 2rem;
+          margin-left: 2rem;
+        }
+
+        .error {
+          font-size: 8rem;
+          text-align: center;
+          opacity: 0.5;
+        }
+
+        .link {
+          position: absolute;
+          margin-left: auto;
+          margin-right: auto;
+          cursor: pointer;
+          text-align: center;
+          bottom: 100px;
+          font-size: 0.85rem;
+        }
+      `}</style>
     </>
   );
-};
-
-export default custom404;
+}
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,13 +1,58 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
-const custom500 = () => {
+export default function Custom500() {
+  const router = useRouter();
   return (
     <>
-      <div className='container'>
-        <div className='w-100 h-100 df-c'>500 internal error</div>
+      <div className='container df-c h-100 w-100'>
+        <div className='df-c'>
+          <div className='text-container'>
+            <p className='error tc-h fs-xxl fw-l'>500</p>
+            <p className='info tc-s fs-xxl fw-l'>Server Error</p>
+          </div>
+          <img src='/assets/images/mozgrad.png' alt='' />
+        </div>
+        <p className='link tc-h' onClick={() => router.back()}>
+          Go Back ↩
+        </p>
       </div>
+      <style jsx>{`
+        .container {
+          height: 80vh;
+        }
+
+        .text-container {
+          position: absolute;
+        }
+
+        img {
+          opacity: 0.1;
+          position: relative;
+        }
+        .info {
+          text-align: center;
+          opacity: 0.5;
+          letter-spacing: 2rem;
+          margin-left: 2rem;
+        }
+
+        .error {
+          font-size: 8rem;
+          text-align: center;
+          opacity: 0.5;
+        }
+
+        .link {
+          position: absolute;
+          margin-left: auto;
+          margin-right: auto;
+          cursor: pointer;
+          text-align: center;
+          bottom: 100px;
+          font-size: 0.85rem;
+        }
+      `}</style>
     </>
   );
-};
-
-export default custom500;
+}
